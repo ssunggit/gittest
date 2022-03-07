85787f4d32778cb268fc47c69c1b99ec9ef5dd56
ssunggit <gustjd68@gmail.com>
2022-03-07 12:07:18 +0900

    commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Master file");
+		System.out.println("Master lunch");
 	}
 }
